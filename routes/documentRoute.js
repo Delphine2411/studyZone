0c3897ad3571b8dc096a7a0c27af5c8e45e44cc5
Delphine2411 <kpankpand@gmail.com>
2025-04-18 14:21:20 +0100

    add

--- routes/documentRoute.js
+++ routes/documentRoute.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const upload = require('../middlewares/uploadMiddleware');
 const documentController = require('../controllers/documentController');
-const { protect, restrictTo } = require('../middlewares/authMiddleware');
+const { protect } = require('../middlewares/authMiddleware');
 
 // Tous les utilisateurs authentifiés peuvent ajouter
 router.post('/', protect, upload.single('fichierUrl'), documentController.createDocument);
@@ -36,7 +36,7 @@ module.exports = router;
 
 /**
  * @swagger
- * /api/documents:
+ * /documents:
  *   post:
  *     summary: Ajouter un nouveau document
  *     tags: [Documents]
@@ -65,7 +65,7 @@ module.exports = router;
 
 /**
  * @swagger
- * /api/documents:
+ * /documents:
  *   get:
  *     summary: Obtenir tous les documents
  *     tags: [Documents]
@@ -76,7 +76,7 @@ module.exports = router;
 
 /**
  * @swagger
- * /api/documents/{id}:
+ * /documents/{id}:
  *   put:
  *     summary: Mettre à jour un document (enseignant seulement)
  *     tags: [Documents]
@@ -121,4 +121,3 @@ module.exports = router;
  *       200:
  *         description: Document supprimé
  */
-router.post('/', protect, documentController.createDocument);
--- swagger.js
+++ swagger.js
@@ -1,7 +1,3 @@
-// swagger.js
-const swaggerJsDoc = require('swagger-jsdoc');
-const swaggerUi = require('swagger-ui-express');
-
 const options = {
   definition: {
     openapi: '3.0.0',
@@ -14,35 +10,24 @@ const options = {
       {
         url: 'http://localhost:5000/api',
       },
-
       {
         url: 'https://studyzone-4gbd.onrender.com',
       },
     ],
-  },
-
-  components: {
-    securitySchemes: {
-      bearerAuth: {
-        type: 'http',
-        scheme: 'bearer',
-        bearerFormat: 'JWT',
+    components: {
+      securitySchemes: {
+        bearerAuth: {
+          type: 'http',
+          scheme: 'bearer',
+          bearerFormat: 'JWT',
+        },
       },
     },
+    security: [
+      {
+        bearerAuth: [],
+      },
+    ],
   },
-  security: [
-    {
-      bearerAuth: [],
-    },
-  ],
-  
-  apis: ['./routes/*.js'], // Tu peux pointer vers tous tes fichiers de routes
+  apis: ['./routes/*.js'], // Tous les fichiers de routes
 };
-
-const swaggerSpec = swaggerJsDoc(options);
-
-const setupSwagger = (app) => {
-  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
-};
-
-module.exports = setupSwagger;
